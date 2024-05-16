5f82a6dad787aa4363a0daab1746b13bcf2612fc
Informatica <japen-2023128@kinal.edu.gt>
2024-05-16 17:21:55 -0600

    Agregamos Productos

--- src/org/javierapen/bean/Productos.java
+++ src/org/javierapen/bean/Productos.java
@@ -10,5 +10,102 @@ package org.javierapen.bean;
  * @author informatica
  */
 public class Productos {
+    private String codigoProducto;
+    private String descripcionProducto;
+    private double precioUnitario;
+    private double precioDocena;
+    private double precioMayor;
+    private String imagenProducto;
+    private int existencia;
+    private int tipoProducto;
+    private int proveedores;
+
+    public Productos() {
+    }
+
+    public Productos(String codigoProducto, String descripcionProducto, double precioUnitario, double precioDocena, double precioMayor, String imagenProducto, int existencia, int tipoProducto, int proveedores) {
+        this.codigoProducto = codigoProducto;
+        this.descripcionProducto = descripcionProducto;
+        this.precioUnitario = precioUnitario;
+        this.precioDocena = precioDocena;
+        this.precioMayor = precioMayor;
+        this.imagenProducto = imagenProducto;
+        this.existencia = existencia;
+        this.tipoProducto = tipoProducto;
+        this.proveedores = proveedores;
+    }
+
+    public String getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(String codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public String getDescripcionProducto() {
+        return descripcionProducto;
+    }
+
+    public void setDescripcionProducto(String descripcionProducto) {
+        this.descripcionProducto = descripcionProducto;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public double getPrecioDocena() {
+        return precioDocena;
+    }
+
+    public void setPrecioDocena(double precioDocena) {
+        this.precioDocena = precioDocena;
+    }
+
+    public double getPrecioMayor() {
+        return precioMayor;
+    }
+
+    public void setPrecioMayor(double precioMayor) {
+        this.precioMayor = precioMayor;
+    }
+
+    public String getImagenProducto() {
+        return imagenProducto;
+    }
+
+    public void setImagenProducto(String imagenProducto) {
+        this.imagenProducto = imagenProducto;
+    }
+
+    public int getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(int existencia) {
+        this.existencia = existencia;
+    }
+
+    public int getTipoProducto() {
+        return tipoProducto;
+    }
+
+    public void setTipoProducto(int tipoProducto) {
+        this.tipoProducto = tipoProducto;
+    }
+
+    public int getProveedores() {
+        return proveedores;
+    }
+
+    public void setProveedores(int proveedores) {
+        this.proveedores = proveedores;
+    }
+    
     
 }
--- src/org/javierapen/controller/MenuProductosController.java
+++ src/org/javierapen/controller/MenuProductosController.java
@@ -5,10 +5,99 @@
  */
 package org.javierapen.controller;
 
+import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+import org.javierapen.bean.Productos;
+import org.javierapen.bean.Proveedor;
+import org.javierapen.bean.TipoProducto;
+import org.javierapen.db.Conexion;
+import org.javierapen.system.Main;
+
 /**
  *
  * @author informatica
  */
-public class MenuProductosController {
-    
+public class MenuProductosController implements Initializable {
+
+    private enum operaciones {
+        AGREGAR, ELIMINAR, EDITAR, ACTUALIZAR, CANCELAR, NINGUNO
+    }
+    private operaciones tipoDeOperaciones = operaciones.NINGUNO;
+
+    private ObservableList<Productos> listaProductos;
+    private ObservableList<TipoProducto> listaTipoProducto;
+    private ObservableList<Proveedor> listaProveedor;
+    private Main escenarioPrincipal;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public ObservableList<Productos> getProducto() {
+        ArrayList<Productos> lista = new ArrayList<Productos>();
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("{call sp_listarProductos()}");
+            ResultSet resultado = procedimiento.executeQuery();
+            while (resultado.next()) {
+                lista.add(new Productos(resultado.getString("codigoProducto"),
+                        resultado.getString("descripcionProducto"),
+                        resultado.getDouble("precioUnitario"),
+                        resultado.getDouble("precioDocena"),
+                        resultado.getDouble("precioMayor"),
+                        resultado.getString("imagenProducto"),
+                        resultado.getInt("existencia"),
+                        resultado.getInt("tipoProducto"),
+                        resultado.getInt("proveedores")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listaProductos = FXCollections.observableArrayList(lista);
+    }
+
+    public ObservableList<TipoProducto> getTipoProducto() {
+        ArrayList<TipoProducto> lista = new ArrayList();
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("{call sp_ListarTipoProducto()}");
+            ResultSet resultado = procedimiento.executeQuery();
+            while (resultado.next()) {
+                lista.add(new TipoProducto(resultado.getInt("codigoTipoProducto"),
+                        resultado.getString("descripcion")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listaTipoProducto = FXCollections.observableArrayList(lista);
+    }
+
+    public ObservableList<Proveedor> getProveedores() {
+        ArrayList<Proveedor> lista = new ArrayList();
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("{call sp_ListarProveedores()}");
+            ResultSet resultado = procedimiento.executeQuery();
+            while (resultado.next()) {
+                lista.add(new Proveedor(resultado.getInt("codigoProveedor"),
+                        resultado.getString("nitProveedor"),
+                        resultado.getString("nombreProveedor"),
+                        resultado.getString("apellidoProveedor"),
+                        resultado.getString("direccionProveedor"),
+                        resultado.getString("razonSocial"),
+                        resultado.getString("contactoPrincipal"),
+                        resultado.getString("paginaWeb")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listaProveedor = FXCollections.observableArrayList(lista);
+    }
 }
